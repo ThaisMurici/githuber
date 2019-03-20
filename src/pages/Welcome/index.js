fb5efbf71aa1fd2a061600cadf843034aa539389
Thais Moraes <murici.thais@gmail.com>
2019-03-19 21:07:24 -0300

    Styling welcome screen

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import {
+  View, Text, TextInput, TouchableOpacity, StatusBar,
+} from 'react-native';
 
 import styles from './styles';
 
 const Welcome = () => (
   <View style={styles.container}>
-    <Text>Testing</Text>
+    <StatusBar barStyle="light-content" />
+    <Text style={styles.title}>Welcome!</Text>
+    <Text style={styles.text}>Please, provide your GitHub username to continue.</Text>
+
+    <View style={styles.form}>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="Insert your username"
+        underlineColorAndroid="transparent"
+      />
+
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <Text style={styles.buttonText}>Proceed</Text>
+      </TouchableOpacity>
+    </View>
   </View>
 );
 

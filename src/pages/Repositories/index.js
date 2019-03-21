80e94bd14b04c68bf2ce9e96cce0c76dd4f636a8
Thais Moraes <murici.thais@gmail.com>
2019-03-20 22:06:18 -0300

    Adding and styling repositories list

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 import api from '~/services/api';
 
 import {
-  ActivityIndicator, Text, View, AsyncStorage,
+  ActivityIndicator, View, AsyncStorage, FlatList,
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import Header from '~/components/Header';
+import RepositoryItem from './RepositoryItem';
 import styles from './styles';
 
 const TabIcon = ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />;
@@ -34,12 +35,24 @@ export default class Repositories extends Component {
     this.setState({ data, loading: false });
   }
 
-  renderList = () => <Text>List</Text>;
+  renderListItem = ({ item }) => <RepositoryItem repository={item} />
+
+  renderList = () => {
+    const { data } = this.state;
+
+    return (
+      <FlatList
+        data={data}
+        keyExtractor={item => String(item.id)}
+        renderItem={this.renderListItem}
+      />
+    );
+  };
 
   render() {
     const { loading } = this.state;
     return (
-      <View>
+      <View style={styles.container}>
         <Header title="Repositories" />
         {loading ? <ActivityIndicator style={styles.loading} /> : this.renderList()}
       </View>
